6cd40670514d4dda981d697adb4523aea84d6972
Jongeon-Github <117550785+inventor1004@users.noreply.github.com>
2023-12-20 15:23:29 -0500

    Create unit test case of Email SET method (Test valid input setting)

--- UnitTest/CustomerEntityTest.cs
+++ UnitTest/CustomerEntityTest.cs
@@ -12,6 +12,47 @@ namespace UnitTest
     public class CustomerEntityTest
     {
         [TestMethod]
+        /*
+         * Function    : TestSetEmailValidPattern()
+         * Desctription: Test whether the SET method sets the email address properly or not
+         *               >> 1. Valid email address 
+         *               >> 2. Valid email address with allowable special characters
+         *               -* the SetEmail() should return 1 *-
+         * Parameter   : void
+         * Return      : void
+         */
+        public void TestSetEmailValidPattern()
+        {
+            CustomerEntity entity = new CustomerEntity();
+            string validEmail = "validPatern@gmail.com";                        // Valid email address 
+            string validEmailWithSpecialChar = "validPatern._%+-@gmail.com";    // Valid email address with allowable special characters
+
+            try
+            {
+                // Test whether the email address sets successfully
+                int isreturnMinusTwo = entity.SetEmail(validEmail);
+                Assert.IsTrue(isreturnMinusTwo == 1);
+
+                isreturnMinusTwo = entity.SetEmail(validEmailWithSpecialChar);
+                Assert.IsTrue(isreturnMinusTwo == 1);
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail($"An exception occurred: {ex.Message}");
+            }
+        }
+
+        [TestMethod]
+        /*
+         * Function    : TestSetEmailInvalidPattern()
+         * Desctription: Test whether the SET method of Email property catches invalid inputs or not
+         *               >> 1. Invalid email input which doesn't follow the email pattern
+         *               >> 2. Invalid email input which doesn't have email domain part (after '@' ~ before '.')
+         *               >> 3. Invalid email input which doesn't have email top-level domain part (after '.')
+         *               -* the SetEmail() should return -2 *-
+         * Parameter   : void
+         * Return      : void
+         */
         public void TestSetEmailInvalidPattern()
         {
             CustomerEntity entity = new CustomerEntity();
@@ -38,6 +79,5 @@ namespace UnitTest
                 Assert.Fail($"An exception occurred: {ex.Message}");
             }
         }
-
     }
 }
