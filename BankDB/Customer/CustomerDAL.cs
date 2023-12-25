f8854812a7d851b3ca80180100beba324e1ab622
Jongeon-Github <117550785+inventor1004@users.noreply.github.com>
2023-12-25 14:52:27 -0500

    CustoemerDAL class is created
    The purpose of this class is to access the MYSQL Database using the SQL Query command.
    In detail, this class will support the blow functionality:
      1. Retrieve the customer password which is matched with the given input Customer Email(ID) address
      2. Change the customer's current PW when the system verifies the customer identification.
      3. Retrieve all the customer information which is required to compose the Main bank app page and perform the transaction functionality

---
+++ BankDB/Customer/CustomerDAL.cs
@@ -0,0 +1,29 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+
+
+namespace BankDB.Customer
+{
+    internal class CustomerDAL
+    {
+        internal MySqlConnection Connection;
+        internal MySqlCommand Command;
+        internal MySqlDataReader Reader;
+
+        public CustomerDAL(string connectionString)
+        {
+            /*
+             * 1. Connect to the MySql database based on the given connectionString
+             * 2. Create the instance of the MySql for excuttion of the MySql command later
+             * 3. Connect the command instance and the MySql instance
+             */
+            this.Connection = new MySqlConnection(connectionString);   
+            this.Command = new MySqlCommand();
+            Command.Connection = Connection;
+        }
+
+
+
+    }
+}
